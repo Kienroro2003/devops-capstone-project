f0a7a34e49a51e2dac2c1852d765ceb522600c39
Kienroro2003 <kienroro281@gmail.com>
2025-09-28 13:05:39 -0400

    fix

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """Returns all of the Accounts"""
@@ -73,7 +74,6 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
